9dd0e9e361fd24a1d572d4df90e26e14ef5c3351
MilindiSenevirathne <milindianu@gmail.com>
2023-08-17 21:51:52 +0530

    added common button

---
+++ src/components/common/CommonButton.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { TouchableOpacity, Text, View, Alert } from "react-native";
+import { Button } from "react-native-paper";
+
+const CommonButton = ({ lable, icon, commonBtnPress = () => { }, backgroundColor, shadowColor, fontColor, disabled, isCapitalize = true, isShowCoformation = false, conformationMessage = 'areYouSure' }) => {
+
+    const showConformation = () => {
+        if (isShowCoformation) {
+            Alert.alert('confirmation', conformationMessage, [{
+                text: 'cancel',
+                onPress: () => { },
+                style: "cancel"
+            },
+            {
+                text: 'yes',
+                onPress: () => {
+                    commonBtnPress()
+                }
+            }
+            ])
+        }
+        else {
+            commonBtnPress()
+        }
+
+    }
+
+    const renderButton = () => {
+        if (disabled) {
+            return (
+                <TouchableOpacity
+                    style={{
+                        height: 35,
+                        width: "200%",
+                        backgroundColor: '#DFE6F2',
+
+                        // borderRadius: 10,
+
+                        alignSelf: "center",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 20
+                    }}
+                    disabled={disabled}
+                    onPress={() => showConformation()}
+                >
+                    <Button
+                        icon={icon}
+                        color={fontColor}
+                    >
+                        <Text style={{
+                            fontFamily: 'inter',
+                            fontSize: 15,
+                            fontWeight: '800',
+                            textTransform: isCapitalize ? "capitalize" : "none",
+                            color: fontColor ?? '#FFFFFF'
+                        }}>
+                            {lable}
+                        </Text>
+                    </Button>
+                </TouchableOpacity>
+            )
+        } else {
+            return (
+
+                <TouchableOpacity
+                    style={{
+                        height: 35,
+                        width: "200%",
+                        backgroundColor: backgroundColor,
+                        alignSelf: "center",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 20
+                    }}
+                    disabled={disabled}
+                    onPress={() => showConformation()}
+                >
+                    <Button
+                        icon={icon}
+                        color={fontColor ?? '#FFFFFF'}
+                        textColor={fontColor ?? '#FFFFFF'}
+                    >
+                        <Text style={{
+                            fontFamily: 'inter',
+                            fontSize: 15,
+                            fontWeight: '500',
+                            color: fontColor ?? '#FFFFFF',
+                            textTransform: isCapitalize ? "capitalize" : "none",
+                        }}>
+                            {lable}
+                        </Text>
+                    </Button>
+                </TouchableOpacity>
+
+            )
+        }
+    }
+
+    return (
+        <View style={{
+            marginTop: 5,
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            paddingBottom: 15,
+            width: "100%"
+        }}>
+            {renderButton()}
+        </View>
+    );
+};
+
+export { CommonButton };
