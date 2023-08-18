74eee74a7331c9cd7b20b56f6e3c6286006de737
MilindiSenevirathne <milindianu@gmail.com>
2023-08-18 21:21:49 +0530

    added common modal

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!!</Text>
