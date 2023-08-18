d2ccf49cb92d9df2bc7dc45f8c50450cc65d176f
VihanPamudya <vihanpamudya12@gmail.com>
2023-08-18 22:10:35 +0530

    Created choose Service page

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import TextField from './src/components/input/TextField';
-import * as React from 'react';
-import { PaperProvider, Searchbar } from 'react-native-paper';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import TextField from "./src/components/input/TextField";
+import * as React from "react";
+import { PaperProvider, Searchbar, Card } from "react-native-paper";
 import {
   DarkTheme,
   DefaultTheme,
   Provider,
   Surface,
   ThemeProvider,
-} from 'react-native-paper';
-import DropdownField from './src/components/input/DropdownField';
-import CalenderField from './src/components/input/CalendarField';
-import { en, registerTranslation } from 'react-native-paper-dates'
-import SearchField from './src/components/input/SearchField';
-registerTranslation('en', en)
+} from "react-native-paper";
+import DropdownField from "./src/components/input/DropdownField";
+import CalenderField from "./src/components/input/CalendarField";
+import { en, registerTranslation } from "react-native-paper-dates";
+import SearchField from "./src/components/input/SearchField";
+registerTranslation("en", en);
 
 export default function App() {
   const theme = {
@@ -23,15 +23,15 @@ export default function App() {
 
     colors: {
       ...DefaultTheme.colors,
-      primary: '#4C0259',  
+      primary: "#4C0259",
       onPrimaryContainer: "#ffffff",
       onSecondaryContainer: "#ffffff",
-      surfaceVariant: '#ffffff',
+      surfaceVariant: "#ffffff",
       onSurfaceVariant: "#000000cc",
-      secondary: '#CA4255',
-      fieldColor: '#D3D1D1',
+      secondary: "#CA4255",
+      fieldColor: "#D3D1D1",
       elevation: {
-        level2: '#ffffff',
+        level2: "#ffffff",
       },
     },
   };
@@ -39,70 +39,17 @@ export default function App() {
     <PaperProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <View style={styles.container}>
-          <ScrollView >
-          <Text>Sample Text Input</Text>
+          <ScrollView>
           <StatusBar style="auto" />
-          <View style={styles.textfield}>
-            <TextField label="Email" placeholder="Enter Email" />
-          </View>
-          <View style={styles.textfield}>
-            <TextField label="Email" placeholder="Enter Email" />
-          </View>
-
-          <Text>Sample Dropdown Input</Text>
-          <View style={styles.textfield}>
-            <DropdownField
-              list={[
-                {
-                  label: 'Male',
-                  value: 'male',
-                },
-                {
-                  label: 'Female',
-                  value: 'female',
-                },
-              ]}
-              label="Gender"
-            />
-          </View>
-          <View style={styles.textfield}>
-            <DropdownField
-              list={[
-                {
-                  label: 'Male',
-                  value: 'male',
-                },
-                {
-                  label: 'Female',
-                  value: 'female',
-                },
-              ]}
-              label="Gender"
-            />
-          </View>
-          <Text>Sample Calendar Input</Text>
-          <View style={styles.textfield}>
-            <CalenderField
-              label="Birthday"
-            />
-          </View>
-          <View style={styles.textfield}>
-            <CalenderField
-              label="Birthday"
-            />
-          </View>
-
-          <Text>Sample Search Bar</Text>
-          <View style={styles.textfield}>
-            <SearchField />
-          </View>
-          <View style={styles.textfield}>
-            <SearchField />
-          </View>
+            <Text>Choose Services</Text>
+            <Card>
+              <Text style={styles.paragraph}>
+                React Native Card View for Android and IOS using
+                "react-native-paper"
+              </Text>
+            </Card>
           </ScrollView>
         </View>
-        
-        
       </ThemeProvider>
     </PaperProvider>
   );
@@ -111,13 +58,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 60
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 60,
   },
-  textfield: {
-    marginTop: 5,
-    width: 300,
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20,
   },
 });
