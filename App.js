682d391a6b6fd09cebb0db9129c237de66a1a432
VihanPamudya <vihanpamudya12@gmail.com>
2023-08-19 03:10:11 +0530

    Added choose services component

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import TextField from "./src/components/input/TextField";
 import * as React from "react";
-import { PaperProvider, Searchbar, Card } from "react-native-paper";
-import {
-  DarkTheme,
-  DefaultTheme,
-  Provider,
-  Surface,
-  ThemeProvider,
-} from 'react-native-paper';
-import DropdownField from './src/components/input/DropdownField';
-import CalenderField from './src/components/input/CalendarField';
-import { en, registerTranslation } from 'react-native-paper-dates';
-import SearchField from './src/components/input/SearchField';
-import NavBar from './src/components/navbar/NavBar';
-registerTranslation('en', en);
+import { StyleSheet, View, Image, ScrollView } from "react-native";
+import { DefaultTheme, Card, Text, Button } from "react-native-paper";
+import { en, registerTranslation } from "react-native-paper-dates";
+registerTranslation("en", en);
 
 export default function App() {
   const theme = {
@@ -24,105 +12,21 @@ export default function App() {
 
     colors: {
       ...DefaultTheme.colors,
-      primary: '#4C0259',
-      onPrimaryContainer: '#ffffff',
-      onSecondaryContainer: '#ffffff',
-      surfaceVariant: '#ffffff',
-      onSurfaceVariant: '#000000cc',
-      secondary: '#CA4255',
-      fieldColor: '#D3D1D1',
+      primary: "#4C0259",
+      onPrimaryContainer: "#ffffff",
+      onSecondaryContainer: "#ffffff",
+      surfaceVariant: "#ffffff",
+      onSurfaceVariant: "#000000cc",
+      secondary: "#CA4255",
+      fieldColor: "#D3D1D1",
       elevation: {
         level2: "#ffffff",
       },
     },
   };
   return (
-    <PaperProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <View style={styles.container}>
-          <ScrollView>
-            <NavBar isLogged={true}/>
-
-            <Text style={styles.text}>Navbar before login</Text>
-            <NavBar isLogged={false}/>
-
-            <View style={styles.container}>
-              <Text>Sample Text Input</Text>
-              <StatusBar style="auto" />
-              <View style={styles.textfield}>
-                <TextField label="Email" placeholder="Enter Email" />
-              </View>
-              <View style={styles.textfield}>
-                <TextField label="Email" placeholder="Enter Email" />
-              </View>
-
-              <Text>Sample Dropdown Input</Text>
-              <View style={styles.textfield}>
-                <DropdownField
-                  list={[
-                    {
-                      label: 'Male',
-                      value: 'male',
-                    },
-                    {
-                      label: 'Female',
-                      value: 'female',
-                    },
-                  ]}
-                  label="Gender"
-                />
-              </View>
-              <View style={styles.textfield}>
-                <DropdownField
-                  list={[
-                    {
-                      label: 'Male',
-                      value: 'male',
-                    },
-                    {
-                      label: 'Female',
-                      value: 'female',
-                    },
-                  ]}
-                  label="Gender"
-                />
-              </View>
-              <Text>Sample Calendar Input</Text>
-              <View style={styles.textfield}>
-                <CalenderField label="Birthday" />
-              </View>
-              <View style={styles.textfield}>
-                <CalenderField label="Birthday" />
-              </View>
-
-              <Text>Sample Search Bar</Text>
-              <View style={styles.textfield}>
-                <SearchField />
-              </View>
-              <View style={styles.textfield}>
-                <SearchField />
-              </View>
-            </View>
-          </ScrollView>
-        </View>
-      </ThemeProvider>
-    </PaperProvider>
+    <Text>Hi</Text>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 40,
-    width: 360,
-  },
-  paragraph: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 20,
-  },
-});
+
