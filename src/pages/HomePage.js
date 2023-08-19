cb11e278df12e41043c1f300dddb198c40ee8de5
MilindiSenevirathne <milindianu@gmail.com>
2023-08-20 00:06:32 +0530

    initial

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import TextField from '../components/input/TextField';
 import * as React from 'react';
 import { PaperProvider, Searchbar } from 'react-native-paper';
 import {
-    DefaultTheme,
-    Provider,
-    ThemeProvider,
+  DefaultTheme,
+  Provider,
+  ThemeProvider,
 } from 'react-native-paper';
 import DropdownField from '../components/input/DropdownField';
 import CalenderField from '../components/input/CalendarField';
@@ -19,103 +19,86 @@ import SuccessErrorModal from '../components/common/SuccessErrorModal';
 registerTranslation('en', en);
 import { useNavigation } from '@react-navigation/native';
 
+const travellingModes = [
+  {value:'ftl', label:'FTL Drive'},
+  {value:'ele', label:'Space Elevator'},
+  {value:'cruise', label:'Space Cruise'},
+  {value:'teleport', label:'Teleport'}
+]
+
 export default function HomePage({ navigation }) {
 
-    const modalShow = React.useRef(null)
-    //   for testing booking page
-    const nav = useNavigation();
+  const [isBookingDetailsClicked, setBookingDetailsClicked] = React.useState(true)
+  const [isShuttleStatusClicked, setShuttleStatusClicked] = React.useState(false)
 
-    const navigateToSpaceships = () => {
-        nav.navigate('BookingPage', { from: 'Los Angeles, Earth', to: 'Ares Prime, Mars', date: '2023-08-19' });
-    };
-    //    for testing booking page
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                <NavBar isLogged={true} />
+  const modalShow = React.useRef(null)
+  //   for testing booking page
+  const nav = useNavigation();
 
-                <Text style={styles.text}>Navbar before login</Text>
-                <NavBar isLogged={false} />
-
-                <View style={styles.container}>
-                    <Text>Sample Text Input</Text>
-                    <StatusBar style="auto" />
-                    <View style={styles.textfield}>
-                        <TextField label="Email" placeholder="Enter Email" />
-                    </View>
-                    <View style={styles.textfield}>
-                        <TextField label="Email" placeholder="Enter Email" />
-                    </View>
-
-                    <Text>Sample Dropdown Input</Text>
-                    <View style={styles.textfield}>
-                        <DropdownField
-                            list={[
-                                {
-                                    label: 'Male',
-                                    value: 'male',
-                                },
-                                {
-                                    label: 'Female',
-                                    value: 'female',
-                                },
-                            ]}
-                            label="Gender"
-                        />
-                    </View>
-                    <View style={styles.textfield}>
-                        <DropdownField
-                            list={[
-                                {
-                                    label: 'Male',
-                                    value: 'male',
-                                },
-                                {
-                                    label: 'Female',
-                                    value: 'female',
-                                },
-                            ]}
-                            label="Gender"
-                        />
-                    </View>
-                    <Text>Sample Calendar Input</Text>
-                    <View style={styles.textfield}>
-                        <CalenderField label="Birthday" />
-                    </View>
-                    <View style={styles.textfield}>
-                        <CalenderField label="Birthday" />
-                    </View>
-
-                    <Text>Sample Search Bar</Text>
-                    <View style={styles.textfield}>
-                        <SearchField />
-                    </View>
-                    <View style={styles.textfield}>
-                        <SearchField />
-                    </View>
-                </View>
-                <CommonButton lable={'Press Here'} commonBtnPress={() => navigation.navigate('MyBookings')} />
-
-                {/*for testing booking page  */}
-                <Button title="Booking page" onPress={navigateToSpaceships} />
-
-                {/* <SuccessErrorModal isError={true} title={'Payment Successful'} message={'klnknd vcscjhcscvh'} visible={true}/> */}
-            </ScrollView>
+  const navigateToSpaceships = () => {
+    nav.navigate('BookingPage', { from: 'Los Angeles, Earth', to: 'Ares Prime, Mars', date: '2023-08-19' });
+  };
+  
+  const bookAShuttle = () => {
+    return(
+      <View style={{ borderRadius: 50, height: isBookingDetailsClicked ? 500 : 50, borderColor: '#D3D1D1', elevation: 5, borderWidth: 1, padding: 10, width: '90%', alignSelf: 'center', marginTop: 20 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 3 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image source={require('../images/Spaceship.png')} style={{ height: 22, width: 22, marginLeft: 10 }} />
+              <Text style={{ fontSize: 14, fontWeight: '700', marginLeft: 10, marginTop: 2 }}>Book a Space Shuttle</Text>
+            </View>
+            {isBookingDetailsClicked ? (
+              <Image source={require('../images/down-arrow.png')} style={{ height: 20, width: 20, transform: [{rotate: '-180deg'}] }} />
+            ): (
+              <Image source={require('../images/down-arrow.png')} style={{ height: 20, width: 20 }} />
+            )}
+          </View>
+          {isBookingDetailsClicked && (
+              <View style={{ flexDirection: 'column', marginTop: 10 }}>
+              <DropdownField label={'Travelling Mode'} list={travellingModes} />
+            </View>            
+            )}
         </View>
-    );
+    )
+  }
+
+  const shuttleStatus = () => {
+    return (
+      <View style={{ borderRadius: 50, height: 50, borderColor: '#D3D1D1', elevation: 5, borderWidth: 1, padding: 10, width: '85%', alignSelf: 'center', marginTop: 20 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 3 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image source={require('../images/charging-circle.png')} style={{ height: 22, width: 22, marginLeft: 10 }} />
+              <Text style={{ fontSize: 14, fontWeight: '700', marginLeft: 10, marginTop: 2 }}>Spaceship Status</Text>
+            </View>
+            <Image source={require('../images/down-arrow.png')} style={{ height: 20, width: 20 }} />
+          </View>
+        </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <NavBar isLogged={false} />
+        <Image source={require('../images/HomeImage.png')} style={{ width: '100%', height: 160, borderBottomLeftRadius: 30, borderBottomRightRadius: 30 }} />
+        {bookAShuttle()}
+        {/* {shuttleStatus()} */}
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: 40,
-        width: 360,
-    },
-    textfield: {
-        marginTop: 5,
-        width: 300,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+    width: 360,
+  },
+  textfield: {
+    marginTop: 5,
+    width: 300,
+  },
 });
